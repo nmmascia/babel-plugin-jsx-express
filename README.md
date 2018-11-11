4856c0bc209c84397b731918e973e7b2f79dff01
Nicholas Mascia <nmmascia@gmail.com>
2018-11-11 16:35:34 -0500

    Add example

--- README.md
+++ README.md
@@ -5,16 +5,53 @@ Babel plugin to write express applications using jsx.
 # Table of Contents
 
 * [Installation](#installation)
+* [Example](#example)
 
 ## Installation
 
 1. npm install package
   ```bash
-  npm install babel-plugin-jsx-express --save-dev
+  npm install @nmmascia/babel-plugin-jsx-express --save-dev
   ```
 2. Add to your `.babelrc`
   ```
   {
-    "plugins": ["jsx-express"]
+    "plugins": ["@nmmascia/jsx-express"]
   }
   ```
+
+## Example
+
+Before:
+
+```jsx
+const express = require('express');
+
+<app>
+  <get
+    path="/hello-world"
+    callback={(req, res) => {
+      res.send('<p>Hello World!</p>');
+    }}
+  />
+  <listen
+    port={8080}
+    callback={(err) => {
+      if (!err) console.log('started!');
+    }}
+  />
+</app>
+```
+
+After:
+```js
+const express = require('express');
+
+const app = express();
+app.get("/hello-world", (req, res) => {
+  res.send('<p>Hello World!</p>');
+})
+app.listen(8080, err => {
+  if (!err) console.log('started!');
+})
+```
--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
-const babel = require('@babel/core');
-
 const buildApp = require('./builders/app');
 const buildListen = require('./builders/listen');
 const buildMiddleware = require('./builders/middleware');
 const buildRoute = require('./builders/route');
-
-const getElementAttributes = require('./utils/get-element-attributes');
 const getElementName = require('./utils/get-element-name');
 
-const expressJsx = function({ types }) {
+module.exports = function({ types }) {
 	return {
 		inherits: require('@babel/plugin-syntax-jsx').default,
 		visitor: {
@@ -30,40 +26,3 @@ const expressJsx = function({ types }) {
 		}
 	};
 };
-
-const example = `
-const express = require('express');
-
-<app>
-  <route path="/resource">
-    <get
-      callback={(req, res, next) => {
-        res.send(200);
-      }}
-    />
-    <post>
-      {(req, res, next) => {
-        res.send(200);
-      }}
-    </post>
-  </route>
-
-  <get
-    path="/health"
-    callback={(req, res, next) => {
-      res.send(200);
-    }}
-  />
-
-  <get path="/ok">
-    {(req, res, next) => {
-      res.send(200)
-    }}
-  </get>
-
-	<listen port={8080} />
-</app>
-`;
-
-const out = babel.transform(example, { plugins: [ expressJsx ] });
-console.log(out.code);
